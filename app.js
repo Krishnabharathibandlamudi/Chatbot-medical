4055df42460ded0ae2d1ad97c45ac946d08effc7
Krishnabharathibandlamudi <157099347+Krishnabharathibandlamudi@users.noreply.github.com>
2025-06-24 12:11:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { getDepartment } = require("symptomLogic");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/analyze", (req, res) => {
+  const { symptoms } = req.body;
+  const department = getDepartment(symptoms);
+  res.json({ department });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
