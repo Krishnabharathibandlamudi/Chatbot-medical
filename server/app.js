b2fd51fad4db8c8829f757c03d88e809c86b3873
Krishnabharathibandlamudi <157099347+Krishnabharathibandlamudi@users.noreply.github.com>
2025-06-24 12:34:10 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 const { getDepartment } = require("../symptomLogic");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.post("/analyze", (req, res) => {
   const { symptoms } = req.body;
@@ -15,6 +25,7 @@ app.post("/analyze", (req, res) => {
   res.json({ department });
 });
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
